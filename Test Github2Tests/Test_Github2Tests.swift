f20e4c8404051d92c1e555999746c5e4bed9cf98
Boss <bossbeem123@gmail.com>
2018-12-18 11:45:21 +0700

    Initial Commit

---
+++ Test Github2Tests/Test_Github2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Test_Github2Tests.swift
+//  Test Github2Tests
+//
+//  Created by dtc on 18/12/2561 BE.
+//  Copyright © 2561 dtc. All rights reserved.
+//
+
+import XCTest
+@testable import Test_Github2
+
+class Test_Github2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
